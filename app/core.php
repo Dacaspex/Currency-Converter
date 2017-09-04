52862beb1b089a73081bbdf4c3d5b1106b470521
Dacaspex <caspersmits11@outlook.com>
2017-09-04 12:25:14 +0200

    Fixed 2 bugs
    Only logged in users may access the back-end.
    Failsafe for invalid request types

--- app/core.php
+++ app/core.php
@@ -7,10 +7,21 @@
 require_once 'webservices/WebService.php';
 require_once 'webservices/CheapCurrencyConverter.php';
 require_once 'webservices/CurrencyConverter.php';
+require_once 'auth/Auth.php';
+
+// Only logged in users can access the webservice
+if (!Auth::check()) {
+    Auth::redirect(Auth::HOME_URL);
+}
 
 $requestType = $_POST['requestType'];
 $response = [];
 $webService = null;
+
+// Failsafe for when no request type is set
+if (!isset($_POST['requestType'])) {
+    $_POST['requestType'] = 'error';
+}
 
 // Depending on the request type, invoke correct webservice
 switch ($requestType) {
